f1463430359d326d8bcbf6c1251f6631ae5d7320
Michael Hubbard <michael.hubbard999@gmail.com>
2023-10-28 15:06:39 -0400

    Export all scopes on the server

---
+++ DHCPScope.ps1
@@ -0,0 +1,14 @@
+﻿# https://4sysops.com/archives/analyze-dhcp-server-with-powershell/
+# 
+# $DHCPServer = "192.168.10.221"
+# $Exportpath = "DHCPLeases.csv"
+#$ExportpathRes = "DHCPRes.csv"
+# $Scope = "192.168.10.0"
+$ExportpathScope = "DHCPScope.csv"
+
+
+$dhcps = (Get-DhcpServerInDC).DnsName
+$dhcps | foreach-Object {Get-DhcpServerv4Scope -ComputerName $_} |
+    Select-Object -Property ScopeId, SubnetMask, Name, State  |
+    Export-Csv -Path $ExportpathScope -NoTypeInformation |
+    Write-Host
