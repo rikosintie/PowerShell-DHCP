0287a4dc5f3d5ab7e41003150901f2b33656ea52
Michael Hubbard <michael.hubbard999@gmail.com>
2023-10-28 15:04:49 -0400

    Find Free addresses in a scope

---
+++ dhcp-free-addresses.ps1
@@ -0,0 +1,6 @@
+﻿# https://4sysops.com/archives/analyze-dhcp-server-with-powershell/
+# 
+# Export-DhcpServer -ComputerName "192.168.10.221" -File "./dhcpexport.xml" -ScopeId 192.168.10.0,10.112.100.0 -Leases
+# finds free addresses in a scope. Change 21 to find a different number of free addresses.
+DhcpServerv4FreeIPAddress -ComputerName 192.168.10.221 -ScopeId 192.168.10.0 `
+-StartAddress 192.168.10.100 -NumAddress 21
