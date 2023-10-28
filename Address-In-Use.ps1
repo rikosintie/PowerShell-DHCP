2ac225be04a7d3c902760a1427e11f8e4fd719a6
Michael Hubbard <michael.hubbard999@gmail.com>
2023-10-28 15:02:19 -0400

    Export leases including address in the deny filter

---
+++ Address-In-Use.ps1
@@ -0,0 +1,12 @@
+﻿# https://4sysops.com/archives/analyze-dhcp-server-with-powershell/
+
+$Exportpath = "Addresses-in-use.csv"
+
+$dhcps = (Get-DhcpServerInDC).DnsName
+$dhcps | foreach-Object {Get-DhcpServerv4Lease -ComputerName $_} |
+    Select-Object -Property IPAddress, ScopeId,  ClientId, HostName, AddressState, LeaseExpiryTime   |
+    Export-Csv -Path $Exportpath -NoTypeInformation |
+    Write-Host
+Write-Host 
+Write-Host '-------Addresses in the Deny List -------'
+Get-DhcpServerv4Filter -ComputerName $dhcps
